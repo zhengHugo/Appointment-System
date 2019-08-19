cb0f92d481906a18a2b954de61fb7685d5ca32a7
zhengHugo <zhenghugo515@gmial.com>
2019-08-19 17:31:55 +0800

    Solve the first input problem

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ def main():
     # elif (identity == 2):
     #     print(2)
 
-    try:
-        identity = input("1. student \t 2. teacher\n")
-        assert identity == '1' or identity == '2'
-        print(identity)
-    except AssertionError:
-        print("\nWrong input! Please select 1 or 2\n")
+    while True:
+        try:
+            identity = input("1. student \t 2. teacher\n")
+            assert identity == '1' or identity == '2'
+        except AssertionError:
+            print("\nWrong input! Please select 1 or 2\n")
+            continue
+        break
 
 
 if __name__ == "__main__":
